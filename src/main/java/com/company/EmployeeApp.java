595758edb414c79ecd76e93873afcbcdd70851a5
root <root@ip-172-31-11-32.ap-south-2.compute.internal>
2026-05-26 17:03:14 +0000

    comitted

---
+++ src/main/java/com/company/EmployeeApp.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class EmployeeApp {
+
+    public static void main(String[] args) {
+
+        String employeeName = "Giri";
+        int employeeId = 101;
+        String department = "DevOps";
+
+        System.out.println("=================================");
+        System.out.println("      Employee Details");
+        System.out.println("=================================");
+        System.out.println("Employee ID   : " + employeeId);
+        System.out.println("Employee Name : " + employeeName);
+        System.out.println("Department    : " + department);
+        System.out.println("=================================");
+    }
+}
